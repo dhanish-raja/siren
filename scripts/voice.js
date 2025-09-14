fdc747e2e34421d9a6ad9689f2c09461fb31e762
Vikhil <vikhil25@gmail.com>
2025-09-14 20:27:08 +0530

    feat: enable firebase hosting

--- scripts/voice.js
+++ scripts/voice.js
@@ -1,23 +1,26 @@
 let mediaRecorder;
 let audioChunks = [];
 
-document.getElementById("record").onclick = async () => {
-    let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.start();
-
-    mediaRecorder.ondataavailable = e => {
+document.getElementById("record").onclick = async (e) => {
+  e.preventDefault();
+  let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.start();
+  mediaRecorder.ondataavailable = (e) => {
     audioChunks.push(e.data);
-    };
+  };
 };
 
-document.getElementById("stop").onclick = () => {
-    mediaRecorder.stop();
-    mediaRecorder.onstop = () => {
-    let blob = new Blob(audioChunks, { type: 'audio/mp3' });
+document.getElementById("stop").onclick = (e) => {
+  e.preventDefault();
+  mediaRecorder.stop();
+  mediaRecorder.onstop = () => {
+    let blob = new Blob(audioChunks, { type: "audio/mp3" });
     let url = URL.createObjectURL(blob);
+    console.log(url);
     document.getElementById("download").href = url;
     document.getElementById("download").download = "recording.mp3";
     document.getElementById("download").innerText = "Download Recording";
-    };
+  };
+  audioChunks = [];
 };
